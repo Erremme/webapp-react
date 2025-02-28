ccbf3012fd50f91fccc98c1c2142c5665f108882
Erremme <Erremme2.0@gmail.com>
2025-02-28 15:08:40 +0100

    feat add component stars in moviepage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -24,12 +24,12 @@ export default function MoviePage(){
         })
     }
 
-    console.log(movie)
+   
 
     useEffect(fetchMovie, [id, navigate])
     return(
-        <div className="max-w-5xl mx-auto">
-            <Link className="font-bold text-white " to="/">Home <i className="fa-solid fa-house text-red-500 hover:text-red-400"></i></Link>
+        <div className="max-w-5xl mx-auto ">
+            <Link className="font-bold text-blue-950 bg-white p-2 rounded-lg " to="/"><i className="fa-solid fa-house text-blue-950 hover:text-blue-400"></i></Link>
             <div className="grid grid-cols-12 gap-2 shadow-xl my-4">
                 <div className="col-span-12 md:col-span-4">
                     <img src={movie.image} alt={movie.title} />
@@ -37,6 +37,7 @@ export default function MoviePage(){
                 <div className="col-span-12 md:col-span-8 bg-white p-4 space-y-4 rounded-xl">
                    <Heading level={1}>{movie.title}</Heading>
                    <Heading level={2}>{movie.author}</Heading>
+                   <Stars vote={movie.avg_vote} />
                    <Heading level={3}>{movie.genre}</Heading>
                    <Heading level={4}>{movie.author}</Heading>
                    <Paragraf size="md" color="secondary">{movie.release_year}</Paragraf>
